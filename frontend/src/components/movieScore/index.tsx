fd05e71a510be8d6a37609b9fe7f6f4f928bf37e
sdmadruga <sdmadruga@gmail.com>
2022-01-11 23:51:29 -0300

    Movie Card

---
+++ frontend/src/components/movieScore/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.css'
+import MovieStars from "components/movieStars";
+
+function MovieScore(){
+    const score = 3.8;
+    const count = 13;
+    return(
+<div className="dsmovie-score-container">
+    <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+    <MovieStars />
+    <p className="dsmovie-score-count">{count} avaliações</p>
+</div>
+    );
+}
+export default MovieScore
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,34 @@
 import Pagination from "components/Pagination";
+import MovieCard from "components/movieCards";
 import React from "react";
 
-function Listening(){
-    return(
-        <Pagination />
+function Listening() {
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg- col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg- col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg- col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg- col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg- col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg- col-xl-3 mb-3">
+                    </div>   
+                </div>
+            </div>
+        </>
+
     );
 }
 export default Listening;
